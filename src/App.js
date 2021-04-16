0cd8aa2ee7be0e8f00558dbd6a7df354a4e87093
Roman Liush <romanliush24@gmail.com>
2021-04-16 03:49:10 +0300

    task 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+    const [color, setColor] = useState('');
+    const [isDot, setDot] = useState(false);
+    const [width, setWidth] = useState(2);
+
+    const widthArr = [];
+    useEffect(() => {
+        widthArr.push(1);
+        console.log(widthArr, 'EFFECT')
+    }, [width])
+
+    const canvas = document.getElementById('canvas');
+    const colorID = document.getElementById('color');
+    const ctx = canvas.getContext("2d");
+
+    let str = '';
+    const theColor = colorID !== null ? str = colorID.value : str === '';
+
+    let dotted = '';
+    (isDot === false) ? dotted = 0.1 : dotted = 1;
+
+    if (colorID) {
+        colorID.addEventListener("input",  () => {
+            setColor(colorID.value)
+            document.getElementById('canvas').innerHTML = colorID.value;
+        }, false);
+    }
+    for (let i = -100; i < 100; i += dotted) {
+        ctx.fillStyle=color;
+        ctx.fillRect(250 + i * 10,250 - Math.pow(i,2),width,3);
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input className={'colorGraph'}
+             id={'color'}
+             placeholder={'Change color'}
+             type={'color'}
+             onClick={() => {
+                 setColor(theColor);
+             }}
+      />
+
+      <div className={'widthGraph'}
+           onClick={() => {
+               setWidth(width + 1);
+           }}
+      > Increase the width</div>
+
+      <div className={'widthGraph'}
+           onClick={() => {
+               setWidth(width - widthArr.length - 3);
+           }}
+      > Reduce the width</div>
+
+        <div className={'dottedLineGraph'}
+           onClick={() => {
+               setDot(prevState => !prevState);
+           }}
+      >Change line</div>
     </div>
   );
 }
